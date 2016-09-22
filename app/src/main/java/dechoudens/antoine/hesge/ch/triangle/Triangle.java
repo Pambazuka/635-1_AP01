2a51c56793af270c44d5596b07b341cb3c168a00
Antoine De Choudens <antoine.dechoude@hes.adhes.hesge.ch>
2016-09-22 15:53:16 +0200

    first commit

---
+++ app/src/main/java/dechoudens/antoine/hesge/ch/triangle/Triangle.java
@@ -0,0 +1,22 @@
+package dechoudens.antoine.hesge.ch.triangle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Module 635.1 - Programmation
+ * Activité principale
+ *
+ * @author ??? - HEG-Genève
+ * @version Version 1.0
+ */
+public class Triangle extends AppCompatActivity {
+    private 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_triangle);
+    } // onCreate
+
+} // Triangle
