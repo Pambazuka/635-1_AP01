e28fcb368cd7bfd480c673fe10458c60bc0a47cb
Antoine de Choudens <antoine.dechoudens@etu.hesge.ch>
2016-09-25 19:19:11 +0200

    finished

--- app/src/main/java/dechoudens/antoine/hesge/ch/triangle/Triangle.java
+++ app/src/main/java/dechoudens/antoine/hesge/ch/triangle/Triangle.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,6 +21,7 @@ public class Triangle extends AppCompatActivity {
     private TextView edCoteB;
     private TextView edCoteC;
     private TextView edAire;
+    private TextView edInfo;
     private Button btnCalculer;
     private Button btnNouveau;
     private Button btnTerminer;
@@ -36,18 +38,20 @@ public class Triangle extends AppCompatActivity {
     private void addTextListeners() {
         TextWatcher listener = new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
             @Override
             public void afterTextChanged(Editable s) {
-                check();
+                if(btnSetEnabled()){
+                    validate();
+                }
+                else{
+                    edInfo.setText("");
+                    edAire.setText("");
+                }
             }
         };
 
@@ -56,7 +60,7 @@ public class Triangle extends AppCompatActivity {
         edCoteC.addTextChangedListener(listener);
     }
 
-    public void calculer(View v) {
+    public void calculate(View v) {
         double a = Double.parseDouble(edCoteA.getText().toString());
         double b = Double.parseDouble(edCoteB.getText().toString());
         double c = Double.parseDouble(edCoteC.getText().toString());
@@ -64,24 +68,70 @@ public class Triangle extends AppCompatActivity {
         double p = 0.5 * (a + b + c);
         double aire = Math.sqrt(p * (p - a) * (p - b) * (p - c));
 
-
         edAire.setText(String.valueOf(aire));
     }
 
-    private void check() {
+    public void nouveau(View v){
+        edCoteA.setText("");
+        edCoteC.setText("");
+        edCoteB.setText("");
+        edInfo.setText("");
+        edAire.setText("");
+    }
+
+    private boolean btnSetEnabled() {
         if (edCoteA.getText().length() > 0 && edCoteB.getText().toString().length() > 0 && edCoteC.getText().toString().length() > 0) {
             btnCalculer.setEnabled(true);
             btnNouveau.setEnabled(true);
+            return true;
         } else {
             btnCalculer.setEnabled(false);
             btnNouveau.setEnabled(false);
+            return false;
         }
+    }
+
+    private void validate(){
+        double a, b, c;
+
+        a = Double.parseDouble(edCoteA.getText().toString());
+        b = Double.parseDouble(edCoteB.getText().toString());
+        c = Double.parseDouble(edCoteC.getText().toString());
+
+        if(notValid(a, b, c)){
+            edInfo.setText("Triangle invalide");
+        }
+        else{
+            if(isEquilateral(a, b, c)) {
+                edInfo.setText("Triangle équilatérale");
+            }
+            else if(isIsocel(a, b, c)) {
+                edInfo.setText("Triangle isocèle");
+            }
+            else {
+                edInfo.setText("Triangle quelconque");
+            }
+        }
+
+    }
+
+    private boolean isIsocel(double a, double b, double c) {
+        return a == b || b == c || a == c;
+    }
+
+    private boolean isEquilateral(double a, double b, double c){
+        return a == b && b == c;
+    }
+
+    private boolean notValid(double a, double b, double c){
+        return a > b + c || b > a + c || c > a + b;
     }
 
     private void initComp() {
         edCoteA = (TextView) findViewById(R.id.edCoteA);
         edCoteB = (TextView) findViewById(R.id.edCoteB);
         edCoteC = (TextView) findViewById(R.id.edCoteC);
+        edInfo = (TextView) findViewById(R.id.edInfo);
         edAire = (TextView) findViewById(R.id.edAire);
         btnCalculer = (Button) findViewById(R.id.btnCalculer);
         btnNouveau = (Button) findViewById(R.id.btnNouveau);
